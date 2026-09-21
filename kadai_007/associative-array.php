a7e133fb56df51a542049979a989c7e6d40bd23c
Sayuri <233426948+Xi3Epi5@users.noreply.github.com>
2026-09-21 16:11:16 +0900

    PHP基礎編 課題7

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <?php
+    $marchandise = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($marchandise);
+    ?>
+    
+</body>
+</html>
